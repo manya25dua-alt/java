e1846d5546e4465d92352952c3d99503171aea74
manya25dua-alt <manya25.dua@gmail.com>
2026-08-07 19:33:23 +0530

    Uppercase To Lowercase

---
+++ UpperToLower.java
@@ -0,0 +1,7 @@
+public class UpperToLower {
+    public static void main(String[] args){
+        String name="manya";
+        String lower=name.toLowerCase();
+        System.out.println("name  "+lower);
+    }
+}
