aac6b80ae81639dcd025f2d88df8d1ddba932d07
manya25dua-alt <manya25.dua@gmail.com>
2026-09-02 09:36:25 +0530

    Reverse A String

---
+++ reverseString.java
@@ -0,0 +1,11 @@
+public class reverseString {
+    public static void main(String args[]){
+
+        //to reverse the string
+        String str="hello";
+        StringBuilder sb= new StringBuilder(str);
+        System.out.println(sb.reverse());
+
+       
+    }
+}
