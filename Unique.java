5fbe0d664324ed75b71e841b9439c5edf48c4fc2
manya25dua-alt <manya25.dua@gmail.com>
2026-09-17 20:29:07 +0530

    Unique Characters In String

---
+++ Unique.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Unique {
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        HashSet<Character> set = new HashSet<>();
+
+        for (char ch : str.toCharArray()) {
+            set.add(ch);
+        }
+
+        System.out.println("Unique characters: " + set);
+    }
+}
